0e890226b0d3ec99d0a0e3ade2846b6bf534a707
Mauro Lepore <maurolepore@gmail.com>
2019-12-18 09:35:04 -0600

    New portfolio_description
    Closes #23
    Thanks @vintented

---
+++ R/portfolio_description.R
@@ -0,0 +1,14 @@
+#' Description of a generic equity or debt portfolio dataset
+#'
+#' A equity or debt portfolio mapped to the physical assets of financial
+#' securities issuers. The PACTA methodology is applied to the mapped data to
+#' produce a results file with the portfolio's relative alignment with different
+#' climate scenarios for technologies in climate-relevant sectors. The portfolio
+#' results include the emissions factors and the absolute and scaled production
+#' associated with the portfolio's exposure to each technology in
+#' climate-relevant sectors.
+#'
+#' @name portfolio_description
+#' @aliases equity debt
+#' @family generic datasets
+NULL
