d9cbce0b88978212a6aaa76425b6014d9d3abf5f
Mauro Lepore <maurolepore@gmail.com>
2019-12-18 09:59:16 -0600

    New market_description
    Relates to https://github.com/2DegreesInvesting/r2dii.analysis/pull/10#issuecomment-566047068
    Closes #24
    Thanks @vintented

---
+++ R/market_description.R
@@ -0,0 +1,13 @@
+#' Description of a generic equity or debt portfolio dataset
+#'
+#' A equity or debt market portfolio mapped to all physical assets of all
+#' financial securities issuers within a predefined physical or economic scope
+#' The PACTA methodology is applied to the market data to produce a results file
+#' with the market's relative alignment with different climate scenarios for
+#' technologies in climate-relevant sectors. The market results include the
+#' emissions factors and the absolute and scaled production associated with the
+#' market's exposure to each technology in climate-relevant sectors.
+#'
+#' @name market_description
+#' @family generic datasets
+NULL
