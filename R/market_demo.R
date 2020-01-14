4c1049117861f7e3e839e63d14e3eb02a5c0bd9f
Mauro Lepore <maurolepore@gmail.com>
2020-01-14 16:44:25 -0600

    Document market_demo and portfolio_demo

---
+++ R/market_demo.R
@@ -0,0 +1,7 @@
+#' A market dataset for demonstration
+#'
+#' @family demo datasets
+#' @seealso [data_dictionary]
+#' @examples
+#' market_demo
+"market_demo"
---
+++ R/portfolio_demo.R
@@ -0,0 +1,7 @@
+#' A market dataset for demonstration
+#'
+#' @family demo datasets
+#' @seealso [data_dictionary]
+#' @examples
+#' portfolio_demo
+"portfolio_demo"
