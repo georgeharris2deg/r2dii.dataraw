6af664a68ef767c0da08688cfb2a0b3e438ce49c
Mauro Lepore <maurolepore@gmail.com>
2020-01-14 16:12:26 -0600

    Add market and portfolio to data/

--- data-raw/market_portfolio.R
+++ data-raw/market_portfolio.R
@@ -1,17 +1,23 @@
 # Source: @vintented via https://github.com/2DegreesInvesting/r2dii.analysis/
 #   issues/6#issuecomment-561543097
 
-read_and_sanitize <- function(path, sheet) {
-  out <- readxl::read_excel(path, sheet = sheet)
-  out$Year <- as.integer(out$Year)
-  out
-}
+library(readr)
+library(here)
 
-path <- here::here("data-raw", "market_portfolio.xlsx")
-portfolio <- read_and_sanitize(path, sheet = "Portfolio")
-portfolio %>% readr::write_csv("data-raw/portfolio.csv")
-# usethis::use_data(portfolio, overwrite = TRUE)
+specs <- cols(
+  Investor.Name = col_character(),
+  Portfolio.Name = col_character(),
+  Scenario = col_character(),
+  ScenarioGeography = col_character(),
+  Allocation = col_character(),
+  Year = col_integer(),
+  Sector = col_character(),
+  Plan.Sec.EmissionsFactor = col_double(),
+  Scen.Sec.EmissionsFactor = col_double()
+)
 
-market <- read_and_sanitize(path, sheet = "Market")
-market %>% readr::write_csv("data-raw/market.csv")
-# usethis::use_data(market, overwrite = TRUE)
+portfolio <- read_csv(here("data-raw", "portfolio.csv"), col_types = specs)
+usethis::use_data(portfolio, overwrite = TRUE)
+
+market <- read_csv(here("data-raw", "market.csv"), col_types = specs)
+usethis::use_data(market, overwrite = TRUE)
