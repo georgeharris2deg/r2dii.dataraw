d1001d7fae704886f49229341d6453ab59baeaa2
Mauro Lepore <maurolepore@gmail.com>
2020-01-14 17:32:12 -0600

    Update seealso to link _demo with _description
    Here for both market_demo and portfolio_demo

--- R/market_demo.R
+++ R/market_demo.R
@@ -1,7 +1,7 @@
 #' A market dataset for demonstration
 #'
 #' @family demo datasets
-#' @seealso [data_dictionary]
+#' @seealso [data_dictionary] [market_description].
 #' @examples
 #' market_demo
 "market_demo"
--- R/portfolio_demo.R
+++ R/portfolio_demo.R
@@ -1,7 +1,7 @@
 #' A market dataset for demonstration
 #'
 #' @family demo datasets
-#' @seealso [data_dictionary]
+#' @seealso [data_dictionary] [portfolio_description].
 #' @examples
 #' portfolio_demo
 "portfolio_demo"
